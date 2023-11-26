61c243e10511c3c8f00066302ceac80edeb4a7e0
Vasilisa <dmitrieva.vasilisa@gmail.com>
2023-11-26 23:03:25 +0300

    rwlock solution

--- ITMO_DZ_4/ThreadSafeArray.swift
+++ ITMO_DZ_4/ThreadSafeArray.swift
@@ -1,21 +1,80 @@
 import Foundation
 
-class ThreadSafeArray<T>  {
+final class ReadWriteLock {
+    private var rwlock = UnsafeMutablePointer<pthread_rwlock_t>.allocate(capacity: 1)
+
+    init() {
+        let result = pthread_rwlock_init(rwlock, nil)
+        assert(result == 0, "Failed to initialize read-write lock with error: \(result)")
+    }
+
+    func read<T>(_ action: () -> T) -> T {
+        defer {
+            pthread_rwlock_unlock(rwlock)
+        }
+        pthread_rwlock_rdlock(rwlock)
+        return action()
+    }
+
+    func write<T>(_ action: () -> T) -> T {
+        defer {
+            pthread_rwlock_unlock(rwlock)
+        }
+        pthread_rwlock_wrlock(rwlock)
+        return action()
+    }
+
+    deinit {
+        pthread_rwlock_destroy(rwlock)
+        rwlock.deallocate()
+    }
+}
+
+class ThreadSafeArray<T> {
     private var array: [T] = []
+    private let lock = ReadWriteLock()
+
+    func append(_ newElement: T) {
+        lock.write {
+            array.append(newElement)
+        }
+    }
+
+    func remove(at index: Int) {
+        lock.write {
+            if self.array.indices.contains(index) {
+                self.array.remove(at: index)
+            }
+        }
+    }
 }
 
 extension ThreadSafeArray: RandomAccessCollection {
     typealias Index = Int
     typealias Element = T
 
-    var startIndex: Index { return array.startIndex }
-    var endIndex: Index { return array.endIndex }
+    var startIndex: Index {
+        return lock.read {
+            array.startIndex
+        }
+    }
+
+    var endIndex: Index {
+        return lock.read {
+            array.endIndex
+        }
+    }
 
     subscript(index: Index) -> Element {
-        get { return array[index] }
+        return lock.read {
+            array[index]
+        }
     }
 
     func index(after i: Index) -> Index {
-        return array.index(after: i)
+        return lock.read {
+            array.index(after: i)
+        }
     }
 }
+
--- ITMO_DZ_4/main.swift
+++ ITMO_DZ_4/main.swift
@@ -1,9 +1,14 @@
 import Foundation
 
-let ff = ThreadSafeArray<Int>()
-var dd = [1]
-dd.append(1)
-ff.dropFirst()
+//var ff = Array<Int>()
+var ff = ThreadSafeArray<Int>()
+
+for index in 0..<100 {
+    DispatchQueue.global().async {
+        ff.append(index)
+        print(ff.count)
+    }
+}
+sleep(100)
 
 let sd = ff[0]
-
